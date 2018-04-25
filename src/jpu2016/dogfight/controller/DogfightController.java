6ee31b852798a25faac52191a9ddbf9bb6d206cf
user <user@10.64.128.104>
2018-04-25 17:17:31 +0200

    Phase 3

---
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -0,0 +1,37 @@
+package jpu2016.dogfight.controller;
+
+public class DogfightController implements IOrderPerfomer{
+	private static int TIME_SLEEP = 30;
+	
+	public DogfightController(DogfightModel dogfightModel) {
+		
+	}
+	
+	public void orderPerform(UserOrder userOrder) {
+		
+	}
+	
+	public void play() {
+		
+	}
+	
+	public void setViewSystem(IViewSystem iViewSystem) {
+		
+	}
+	
+	private void launchMissile(int player) {
+		
+	}
+	
+	private void gameLoop() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
