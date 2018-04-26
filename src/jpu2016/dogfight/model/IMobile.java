f0a40542845788dc671cccaf0b9d191d3acf1d37
user <user@10.64.128.104>
2018-04-26 14:12:30 +0200

    Phase 2

---
+++ src/jpu2016/dogfight/model/IMobile.java
@@ -0,0 +1,18 @@
+package jpu2016.dogfight.model;
+
+public interface IMobile {
+	Direction getDirection();
+	void setDirection(Direction direction);
+	Point getPosition();
+	Dimension getDimension();
+	int getWidth();
+	int getHeight();
+	int getSpeed();
+	Image getImage();
+	void move();
+	void placeInArea(IArea area);
+	boolean isPlayer(int player);
+	void setDogfightModel(DogfightModel dogfightModel);
+	boolean hit();
+	boolean isWeapon();
+}
