be6f8a9c7d3554e7a2506761eadb081aeaae60b6
leoco <leoco@DESKTOP-J6780UH>
2018-04-27 11:01:30 +0200

    Phase 3

---
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -0,0 +1,24 @@
+package jpu2016.dogfight.view;
+import java.lang.*;
+import java.util.Observable;
+
+import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.model.IDogfightModel;
+
+
+public class DogfightView implements IViewSystem, Runnable{
+	public DogfightView(IOrderPerformer orderPerfomer, IDogfightModel dogfightModel, Observable observable) {
+	}
+	public void run() {
+		
+	}
+	public void displayMessage(String message) {
+		
+		
+	}
+	public void closeAll() {
+		
+	}
+
+
+}
